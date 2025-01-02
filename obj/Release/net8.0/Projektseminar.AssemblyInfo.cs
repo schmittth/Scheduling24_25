47f44d762d17f628386baba7e48dfb34cbe09a0e
schmittth <tommi-schmitt@web.de>
2025-01-02 13:31:42 +0100

    Tests Done

--- obj/Release/net8.0/Projektseminar.AssemblyInfo.cs
+++ obj/Release/net8.0/Projektseminar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projektseminar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8559a579b5c83e8de7824c0a993c49345a9ad458")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b3d18babf149ba0f581a7b9e393053e35bf07e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projektseminar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projektseminar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
