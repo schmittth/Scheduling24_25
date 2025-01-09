ec56566abad4cd679082797727c97428a81f17d4
haeusslerba <haeusslerba98469@th-nuernberg.de>
2025-01-09 03:04:05 +0100

    a

--- Main.cs
+++ Main.cs
@@ -14,131 +14,89 @@ namespace Projektseminar
         //Methoden
         public static void Main(string[] args)
         {
-            Stopwatch stopwatch = new Stopwatch(); //Initialisiere eine Stopwatch um die Laufzeit zu messen.
-            int unixTimestamp = (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds; //Generiere sog. Unix-Timestamp.
             int seedValue = 0; //Initialisiere eine Int der den Random-Seed für diese Ausführung entält. Setze auf 0
-            int seedChoice = 0;
 
-            string instanceChoice = Dialog.ChooseInstance(); //Bestimme ob randomisierte oder feste Instanz        
+            foreach (string subDir in Directory.GetDirectories("../../../Diagramms/", "ClassroomInstance*"))
+            {
+                int unixTimestamp = (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds; //Generiere sog. Unix-Timestamp.
 
-            Tuple<int, int, int, int, int> randomInstanceSize = null;
-            List<string> subFiles = new List<string>();
-            int instanceAmount = 0;
+                List<string> subFiles = new List<string>();
+                int instanceAmount = 0;
 
-            if (instanceChoice == "Random") //Wenn randomisierte Instanz lasse Instanzgröße auswählen
-            {
-                instanceAmount = Dialog.ChooseInstanceAmount(); //Bestimme wie oft Instanzen generiert werden sollen   
-                randomInstanceSize = Dialog.ChooseRandomInstanceSize(); //Größe der randomisierten Instanz auswählen               
-            }
-            else if (instanceChoice.EndsWith(".txt")) //When ein .txt-File selektiert wird nur eine Instanz geladen
-            {
-                instanceAmount = 1;
-            }
-            else if (instanceChoice.EndsWith("\\"))
-            {
-                foreach (string subDir in Directory.GetDirectories(instanceChoice))
-                {
-                    foreach (string subFile in Directory.GetFiles(subDir, "*.txt", SearchOption.TopDirectoryOnly))
-                    {
-                        subFiles.Add(subFile);
-                    }
-                }
+                subFiles = Directory.GetFiles(subDir, "*.txt", SearchOption.TopDirectoryOnly).ToList();
                 instanceAmount = subFiles.Count;
-            }
-            else
-            {
-                subFiles = Directory.GetFiles(instanceChoice, "*.txt", SearchOption.TopDirectoryOnly).ToList();
-                instanceAmount = subFiles.Count;
-            }
 
-            int solverChoice = Dialog.ChooseSolver(); //Lasse Lösungsansatz auswählen
-            string priorityRule = ""; //Initialisiere PriortityRule String
-            string neighboorhood = ""; //Initialisiere Nachbarschafts String
+                int solverChoice = 2;
+                string priorityRule = ""; //Initialisiere PriortityRule String
+                string neighboorhood = ""; //Initialisiere Nachbarschafts String
 
-
-            if (solverChoice == 2 || solverChoice == 3)
-            {
-                //priorityRule = Dialog.ChoosePriorityRule();
-                priorityRule = "LTT";
-                neighboorhood = Dialog.ChooseNeighboorhood();
-            }
-
-            Tuple<double, int> simAnnealParams = null;
-            if (solverChoice == 2)
-            {
-                seedChoice = Dialog.SeedAlgorithm();
-                simAnnealParams = Dialog.ChooseSimAnnealParameters();
-            }
-
-            //
-            for (int instanceCounter = 0; instanceCounter < instanceAmount; instanceCounter++)
-            {
-                if (seedChoice == 0)
-                {
-                    Random randSeed = new Random();
-                    seedValue = randSeed.Next(0, Int32.MaxValue);
-                }
-                else
-                {
-                    seedValue = seedChoice;
-                }
-
-                Importer importer = new Importer();
-                if (instanceChoice == "Random")
-                {
-                    importer.ImportRandomInstance(randomInstanceSize.Item1, randomInstanceSize.Item2, randomInstanceSize.Item3, randomInstanceSize.Item4, randomInstanceSize.Item5);
-                }
-                else if (instanceChoice.EndsWith(".txt"))
-                {
-                    importer.ImportInstanceFromFile(instanceChoice);
-                }
-                else
-                {
-                    importer.ImportInstanceFromFile(subFiles[instanceCounter]);
-                }
-
-                Problem problem = importer.GenerateProblem();
-               
-                GifflerThompson gifflerThompson = new GifflerThompson(problem, priorityRule);
 
                 if (solverChoice == 2 || solverChoice == 3)
                 {
-                    problem = gifflerThompson.InitialSolution();
-                    problem.ProblemAsDiagramm($@"..\..\..\Diagramms\{unixTimestamp}\instance{instanceCounter}\initialSolution.html", false, seedValue, stopwatch.Elapsed);
+                    priorityRule = "LTT";
+                    neighboorhood = "N3";
                 }
 
-                switch (solverChoice) //Switch-Case Anweisungen basierend auf der Solver-Auswahl.
+                Tuple<double, int> simAnnealParams = null;
+                if (solverChoice == 2)
                 {
-                    //Solver: Google OR-Tools
-                    case 1:
-                        //Erstelle neues OR-Solver Objekt und löse das Problem.
-                        ORToolsSolver.GoogleOR googleor = new ORToolsSolver.GoogleOR(problem);
-                        problem = googleor.DoORSolver();
+                    simAnnealParams = Tuple.Create(0.99, 25000);
+                }
 
-                        googleor.Log(instanceChoice, seedValue, googleor.Stopwatch.Elapsed, "GoogleOR"); //Logge die Ausführung
-                        problem.ProblemAsDiagramm($@"..\..\..\Diagramms\{unixTimestamp}\instance{instanceCounter}\googleOr.html", true, seedValue, googleor.Stopwatch.Elapsed);
+                //
+                for (int instanceCounter = 0; instanceCounter < instanceAmount; instanceCounter++)
+                {
 
-                        break;
-                    //Solver: Simulated Annealing
-                    case 2:
-                        SimulatedAnnealing simAnneal = new SimulatedAnnealing(problem, simAnnealParams.Item1, simAnnealParams.Item2, neighboorhood);
-                        problem = simAnneal.DoSimulatedAnnealing(seedValue);
+                    Random randSeed = new Random();
+                    seedValue = randSeed.Next(0, Int32.MaxValue);
 
-                        simAnneal.Log(instanceChoice, seedValue, simAnneal.Stopwatch.Elapsed, "Simulated Annealing", simAnneal.CoolingFactor, simAnneal.Iterations, simAnneal.Neighboorhood, gifflerThompson.PriorityRule);
-                        problem.ProblemAsDiagramm($@"..\..\..\Diagramms\{unixTimestamp}\instance{instanceCounter}\simAnneal.html", true, seedValue, simAnneal.Stopwatch.Elapsed);
-                        //problem.ProblemAsFile($@"..\..\..\Diagramms\{unixTimestamp}\instance{instanceCounter}\instanceExport.txt");
-                        break;
-                    case 3:
-                        LocalSearch.LocalSearch localSearch = new LocalSearch.LocalSearch(problem, neighboorhood);
-                        problem = localSearch.DoLocalSearch();
+                    Importer importer = new Importer();
+                    importer.ImportInstanceFromFile(subFiles[instanceCounter]);
 
-                        localSearch.Log(instanceChoice, seedValue, stopwatch.Elapsed, "Local Search", iterations: 0, priorityRule: gifflerThompson.PriorityRule);
-                        problem.ProblemAsDiagramm($@"..\..\..\Diagramms\{unixTimestamp}\instance{instanceCounter}\localSearch.html", true, seedValue, localSearch.Stopwatch.Elapsed);
+                    Problem problem = importer.GenerateProblem();
 
-                        break;
+                    GifflerThompson gifflerThompson = new GifflerThompson(problem, priorityRule);
+
+                    if (solverChoice == 2 || solverChoice == 3)
+                    {
+                        problem = gifflerThompson.InitialSolution();
+                        problem.ProblemAsDiagramm($@"..\..\..\Diagramms\{unixTimestamp}\instance{instanceCounter}\initialSolution.html", false, seedValue, gifflerThompson.Stopwatch.Elapsed);
+                    }
+
+                    switch (solverChoice) //Switch-Case Anweisungen basierend auf der Solver-Auswahl.
+                    {
+                        //Solver: Google OR-Tools
+                        case 1:
+                            //Erstelle neues OR-Solver Objekt und löse das Problem.
+                            ORToolsSolver.GoogleOR googleor = new ORToolsSolver.GoogleOR(problem);
+                            problem = googleor.DoORSolver();
+
+                            //googleor.Log(instanceChoice, seedValue, googleor.Stopwatch.Elapsed, "GoogleOR"); //Logge die Ausführung
+                            problem.ProblemAsDiagramm($@"..\..\..\Diagramms\{unixTimestamp}\instance{instanceCounter}\googleOr.html", true, seedValue, googleor.Stopwatch.Elapsed);
+
+                            break;
+                        //Solver: Simulated Annealing
+                        case 2:
+                            SimulatedAnnealing simAnneal = new SimulatedAnnealing(problem, simAnnealParams.Item1, simAnnealParams.Item2, neighboorhood);
+                            problem = simAnneal.DoSimulatedAnnealing(seedValue);
+
+                            simAnneal.Log(subFiles[instanceCounter], seedValue, simAnneal.Stopwatch.Elapsed, "Simulated Annealing", simAnneal.CoolingFactor, simAnneal.Iterations, simAnneal.Neighboorhood, gifflerThompson.PriorityRule);
+                            problem.ProblemAsDiagramm($@"..\..\..\Diagramms\{unixTimestamp}\instance{instanceCounter}\simAnneal.html", false, seedValue, simAnneal.Stopwatch.Elapsed);
+                            //problem.ProblemAsFile($@"..\..\..\Diagramms\{unixTimestamp}\instance{instanceCounter}\instanceExport.txt");
+                            break;
+                        case 3:
+                            LocalSearch.LocalSearch localSearch = new LocalSearch.LocalSearch(problem, neighboorhood);
+                            problem = localSearch.DoLocalSearch();
+
+                            //localSearch.Log(instanceChoice, seedValue, localSearch.Stopwatch.Elapsed, "Local Search", iterations: 0, priorityRule: gifflerThompson.PriorityRule);
+                            problem.ProblemAsDiagramm($@"..\..\..\Diagramms\{unixTimestamp}\instance{instanceCounter}\localSearch.html", true, seedValue, localSearch.Stopwatch.Elapsed);
+
+                            break;
+                    }
                 }
             }
-            stopwatch.Stop();
+
+
         }
     }
 }
--- MetaHeuristic/SimulatedAnnealing.cs
+++ MetaHeuristic/SimulatedAnnealing.cs
@@ -1,4 +1,5 @@
 ﻿using Projektseminar.Instance;
+using System.ComponentModel.Design;
 
 namespace Projektseminar.MetaHeuristic
 {
@@ -35,60 +36,60 @@ namespace Projektseminar.MetaHeuristic
                 //CurrentProblem = BestProblem; //Alternative mit bestem Problem weitermachen
 
                 //Iteriere über die Anzahl an Iterationen
-                for (int i = 0; i < Iterations; i++)
+                for (int i = 0; i < Iterations && Stopwatch.Elapsed.TotalSeconds < MaxRuntimeInSeconds; i++)
                 {
-                    Dictionary<int, List<Tuple<Instance.Task, Instance.Task, Machine>>> dict = CurrentProblem.GetNeighboorhood(Neighboorhood); //Instanziiere Dict mit Nachbarschaften
-                    List<int> cyclicNeighbors = new List<int>(); //Erstelle Liste mit zkylischen Nachbarschaften
+                        Dictionary<int, List<Tuple<Instance.Task, Instance.Task, Machine>>> dict = CurrentProblem.GetNeighboorhood(Neighboorhood); //Instanziiere Dict mit Nachbarschaften
+                        List<int> cyclicNeighbors = new List<int>(); //Erstelle Liste mit zkylischen Nachbarschaften
 
-                    //Iteriere bis eine nicht-zyklische Lösung gefunden wurde
-                    do
-                    {
-                        newProblem = new Problem(CurrentProblem); //Kopiere aktuelles Problem
-                        int chooseNeighbor;
-
-                        //Iteriere solange bis ein nicht-zyklischer Nachbar gewählt wurde
+                        //Iteriere bis eine nicht-zyklische Lösung gefunden wurde
                         do
                         {
-                            chooseNeighbor = random.Next(0, dict.Count); //Wähle Zufallszahl zwischen 0 und allen Nachbarschaften
-                            /*if (cyclicNeighbors.Count > 1)
+                            newProblem = new Problem(CurrentProblem); //Kopiere aktuelles Problem
+                            int chooseNeighbor;
+
+                            //Iteriere solange bis ein nicht-zyklischer Nachbar gewählt wurde
+                            do
                             {
-                                string cyclicString = cyclicNeighbors.ToString();
-                                Console.WriteLine($"Invalids: {cyclicString}");
-                            }*/
-                        } 
-                        while (cyclicNeighbors.Contains(chooseNeighbor));
-                        cyclicNeighbors.Add(chooseNeighbor); //Füge ausgewählte Nachbarschaft, Liste hinzu
+                                chooseNeighbor = random.Next(0, dict.Count); //Wähle Zufallszahl zwischen 0 und allen Nachbarschaften
+                                /*if (cyclicNeighbors.Count > 1)
+                                {
+                                    string cyclicString = cyclicNeighbors.ToString();
+                                    Console.WriteLine($"Invalids: {cyclicString}");
+                                }*/
+                            }
+                            while (cyclicNeighbors.Contains(chooseNeighbor));
+                            cyclicNeighbors.Add(chooseNeighbor); //Füge ausgewählte Nachbarschaft, Liste hinzu
 
-                        //Führe alle Tauschschritte aus
-                        foreach (Tuple<Instance.Task, Instance.Task, Machine> tuple in dict[chooseNeighbor])
-                        {
-                            newProblem.SwapTasks(tuple.Item1, tuple.Item2, tuple.Item3);
+                            //Führe alle Tauschschritte aus
+                            foreach (Tuple<Instance.Task, Instance.Task, Machine> tuple in dict[chooseNeighbor])
+                            {
+                                newProblem.SwapTasks(tuple.Item1, tuple.Item2, tuple.Item3);
+                            }
                         }
-                    }
-                    while (!newProblem.CheckCyclicity());
+                        while (!newProblem.CheckCyclicity());
 
-                    //Wenn neuer Makespan besser als aktueller, ersetze aktuelles Problem
-                    if (CurrentProblem.Makespan > newProblem.Makespan)
-                    {
-                        //Wenn neuer Makespan besser als bester, ersetze bestes Problem
-                        if (BestProblem.Makespan > newProblem.Makespan)
+                        //Wenn neuer Makespan besser als aktueller, ersetze aktuelles Problem
+                        if (CurrentProblem.Makespan > newProblem.Makespan)
                         {
-                            BestProblem = newProblem;
-                        }
-                        CurrentProblem = newProblem;
-                    }
-                    else
-                    {
-                        int delta = newProblem.Makespan - CurrentProblem.Makespan; //Berechne ganzzahlige Differenz zwischen aktuellem und neuem Makespan als Delta
-                        double exponent = -delta / Temperature; //Berechne Quotient aus Delta und aktueller Temperatur als Gleitkommazahl
-                        double probability = Math.Pow(Math.E, exponent); //Berechne Wahrscheinlichkeit aus eulersche Konstante hoch berechnetem Exponent
-                        
-                        //Akzeptiere geringeren Makespan mit berechneter Zufallswahrscheinlichkeot
-                        if (random.NextDouble() < probability)
-                        {
+                            //Wenn neuer Makespan besser als bester, ersetze bestes Problem
+                            if (BestProblem.Makespan > newProblem.Makespan)
+                            {
+                                BestProblem = newProblem;
+                            }
                             CurrentProblem = newProblem;
                         }
-                    }
+                        else
+                        {
+                            int delta = newProblem.Makespan - CurrentProblem.Makespan; //Berechne ganzzahlige Differenz zwischen aktuellem und neuem Makespan als Delta
+                            double exponent = -delta / Temperature; //Berechne Quotient aus Delta und aktueller Temperatur als Gleitkommazahl
+                            double probability = Math.Pow(Math.E, exponent); //Berechne Wahrscheinlichkeit aus eulersche Konstante hoch berechnetem Exponent
+
+                            //Akzeptiere geringeren Makespan mit berechneter Zufallswahrscheinlichkeot
+                            if (random.NextDouble() < probability)
+                            {
+                                CurrentProblem = newProblem;
+                            }
+                        }                 
                 }
                 Temperature = Temperature * CoolingFactor; //Reduziere Temperatur entsprechend des Abkühlungsfaktors
             }
