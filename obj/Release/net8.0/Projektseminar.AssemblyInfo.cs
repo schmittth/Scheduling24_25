690b6d58b8850f43afe9f89281e7149a586c839d
haeusslerba <haeusslerba98469@th-nuernberg.de>
2025-01-12 03:20:26 +0100

    m

--- obj/Release/net8.0/Projektseminar.AssemblyInfo.cs
+++ obj/Release/net8.0/Projektseminar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projektseminar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6495de17e72aa7e1e9f7998781661cc4eded7afd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81aed433445ec8fea3584b7fbe693c6570ea64f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projektseminar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projektseminar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
