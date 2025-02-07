423ce3e756f5df92a4ca2e3a75c5957a13655267
haeusslerba <haeusslerba98469@th-nuernberg.de>
2025-02-07 10:44:19 +0100

    Set main to fixed parameters

--- obj/Debug/net8.0/Projektseminar.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projektseminar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projektseminar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ded4a9da0da7a9d4d9b92570a1b8b6c978569478")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projektseminar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projektseminar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
