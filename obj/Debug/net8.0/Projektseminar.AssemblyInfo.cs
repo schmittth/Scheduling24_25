411b6613eac2e608a641f07fe7ca932731288f29
haeusslerba <haeusslerba98469@th-nuernberg.de>
2025-02-07 10:40:14 +0100

    Ready for send off

--- obj/Debug/net8.0/Projektseminar.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projektseminar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projektseminar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfd4a58438e9f8eb5f67a9d5b32a1291892aeb7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b17888a3c0d5b4d9580b072bb877d1249249b4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projektseminar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projektseminar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
